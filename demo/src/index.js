25508dc29c7dc7884d0b29577c6725f892398d4b
Kim Andre Furevikstrand <lozymon@gmail.com>
2018-11-09 14:58:26 -0300

    test

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,26 +4,27 @@ import {render} from 'react-dom'
 import LmNumber from '../../src'
 
 class Demo extends Component {
-  state = {
-    value: '123',
-  };
+    state = {
+        value: '123',
+    };
 
-  onChange = (value) => {
-    this.setState({value: value});
-  };
+    onChange = (value) => {
+        this.setState({value: value});
+    };
 
-  render() {
-    return <div>
-      <h1>react-number Demo</h1>
-      <LmNumber
-        name="react-number"
-        precision={0}
-        unit="R$"
-        value={this.state.value} 
-        onChange={this.onChange} 
-      />
-    </div>
-  }
+    render() {
+        return <div>
+            <h1>react-number Demo</h1>
+            <LmNumber
+                maxLength={8}
+                name="react-number"
+                precision={2}
+                unit="R$"
+                value={this.state.value}
+                onChange={this.onChange}
+            />
+        </div>
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'));
